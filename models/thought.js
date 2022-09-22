9683127736d9b39e4c87b593db4987290475e34b
Jason South <jason.south@me.com>
2022-09-21 21:57:51 -0500

    completed models

---
+++ models/thought.js
@@ -0,0 +1,67 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: String,
+            default: dateFormat(new Date())
+        }
+    },
+    {        
+        toJSON: {
+            getters: true,
+        },
+        id: false
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1, 
+            maxLength: 280
+        },
+        createdAt: {
+            type: String,
+            required: true,
+        },
+        username: {
+            type: Schema.Types.ObjectId,
+            ref: 'thought'
+        },
+        reactions: [
+            reactionSchema
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const thought = model('thought', thoughtSchema);
+
+module.exports = thought;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,2 +1,16 @@
 const router = require('express').Router();
 
+const { thought } = require('../../models')
+
+router.get('/', async (req, res) => {
+    const allThoughts = await thought.find({})
+    res.json(allThoughts)
+})
+
+router.post('/', async (req, res) => {
+    const newThought = await thought.create(req.body)
+    console.log(newThought)
+    res.send("made a new one")
+})
+
+module.exports = router;
