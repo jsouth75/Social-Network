c336d337d095e23a926c3bf8abe3fe7a743733b1
Jason South <jason.south@me.com>
2022-09-24 22:30:36 -0500

    updating user & thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,15 +1,51 @@
 const router = require('express').Router();
 const { thought } = require('../../models')
 
+// Get all thoughts
 router.get('/', async (req, res) => {
     const allThoughts = await thought.find({})
     res.json(allThoughts)
 })
 
+// Get thought by Id
+router.get('/:thoughtId', async (req, res) => {
+    const thoughtById = await thought.findById({})
+    res.json(thoughtById)
+})
+
+// Create New thought
 router.post('/', async (req, res) => {
-    const newThought = await thought.create(req.body)
-    console.log(newThought)
+    const createNewThought = await thought.create(req.body)
+    console.log(createNewThought)
     res.send("New thought")
+})
+
+// Update Thought
+router.put('/:thoughtId', async (req, res) => {
+    const updateThought = await thought.findOneAndUpdate(req.body)
+    console.log(updateThought)
+    res.send("Updated thought")
+})
+
+// Delete Thought
+router.delete('/:thoughtId', async (req, res) => {
+    const thoughtById = await thought.findOneAndDelete(req.body)
+    console.log(thoughtById)
+    res.send("Thought deleted")
+})
+
+// Create Reaction 
+router.post('/:thougthId/reactions', async (req, res) => {
+    const createReaction = await thought.create(req.body)
+    console.log(createReaction)
+    res.send("New Reaction")
+})
+
+// Delete Reaction
+router.delete('/:thoughtId/reactions/:reactionId', async (req, res) => {
+    const reactionById = await thought.findByIdAndDelete(req.body)
+    console.log(reactionById)
+    res.send("Reaction deleted")
 })
 
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,43 +1,50 @@
 const router = require('express').Router();
 const { user } = require('../../models')
 
+// Get all users
 router.get('/', async (req, res) => {
     const allUsers = await user.find({})
     res.json(allUsers)
 })
 
-router.get('/:userId',async (req, res) => {
-    const userById = await user.findById
+// Get User by Id
+router.get('/:userId', async (req, res) => {
+    const userById = await user.findById({})
     res.json(userById)
 })
 
+// Create New User
 router.post('/', async (req, res) => {
     const newUser = await user.create(req.body)
     console.log(newUser)
     res.send("New user created")
 })
 
+// Update User
 router.put('/:userId', async (req, res) => {
-    const updateUser = await user.updateUser(req.body)
+    const updateUser = await user.findOneAndUpdate(req.body)
     console.log(updateUser)
     res.send("User updated")
 })
 
+// Delete User
 router.delete('/:userId', async (req, res) => {
-    const deleteUser = await user.delelteUser(req.body)
+    const deleteUser = await user.findOneAndDelete(req.body)
     console.log(deleteUser)
     res.send("User deleted")
 })
 
-router.get('/:userId/friends/:friendId', async (req, res) => {
-    const newFriend = await user.findOneAndUpdate
-    (req.body)
+// Create Friend
+router.post('/:userId/friends/:friendId', async (req, res) => {
+    const newFriend = await user.create(req.body)
     console.log(newFriend)
     res.send("New friend added to user's friend list")
 })
 
+// Delete Friend
 router.delete('/:userId/friends/:friendId', async (req, res) => {
-    const deleteFriend = await user.delelteUser(req.body)
+    const deleteFriend = await user.findOneAndDelete
+    (req.body)
     console.log(deleteFriend)
     res.send("Friend removed from user's friend list")
 })
